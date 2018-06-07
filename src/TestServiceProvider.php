6e00a1729e712921f6b89fd4a60a5b773cc15c9a
Ketan <ketanmk@teamvault.net>
2018-06-07 14:04:44 +0530

    17th

--- src/TestServiceProvider.php
+++ src/TestServiceProvider.php
@@ -24,6 +24,6 @@ class TestServiceProvider extends ServiceProvider
     public function register()
     {
         include __DIR__.'/routes/web.php';
-        $this->app->make('vendor\Ketan\Test\TestController');
+        $this->app->make('Ketan\Test\TestController');
     }
 }
